ac2d3def89cac5b55279cf5455f7b5140b5aeb18
Dmitry Krayniy <krdima92@gmail.com>
2022-06-13 15:17:46 +0400

    Removed bash script for gpg

--- java/configure_gpg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export XDG_RUNTIME_DIR="/run/user/$UID"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-
-systemctl --user status gpg-agent
-systemctl --user stop gpg-agent
-systemctl --user start gpg-agent
